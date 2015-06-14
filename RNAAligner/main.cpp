458865d584853df007fa4117f88e852badf7bda6
Josip Maric <josip.maric@fiveminutes.eu>
2015-06-14 22:37:50 +0200

    Initial Commit

---
+++ RNAAligner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RNAAligner
+//
+//  Created by Josip Maric on 14/06/15.
+//  Copyright (c) 2015 Josip Maric. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
